1815679d34be10f75ab3780957a0232901ff56d2
EasternHeart <heartldev@gmail.com>
2013-02-17 21:12:00 +0800

    Modifying...

--- MonitorServer-Qt/mainwindow.cpp
+++ MonitorServer-Qt/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    udpSocket = new QUdpSocket();
+    bool conn = udpSocket->bind(9741,QUdpSocket::ShareAddress);
+    if (!conn)
+    {
+        QMessageBox::information(NULL,"Fatal Erorr","Cannot bind port 9741", QMessageBox::Abort);
+    }
 }
 
 MainWindow::~MainWindow()
--- MonitorServer-Qt/mainwindow.h
+++ MonitorServer-Qt/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include <QtNetwork/QtNetwork>
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +17,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QUdpSocket *udpSocket;
 };
 
 #endif // MAINWINDOW_H
